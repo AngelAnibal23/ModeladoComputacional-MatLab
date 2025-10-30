c7df1d88732d8031ea4a4952b37e116a65d5d5c2
Guts <aamamanica@unjbg.edu.pe>
2025-10-30 10:22:40 -0500

    Update LAGRANG1.m

--- LAGRANG1.m
+++ LAGRANG1.m
@@ -1,26 +1,50 @@
-function gpt()
-    n = input('Ingrese el numero de datos que va ingresar: '); 
-    xr = input('Ingrese la abscisa intermedia que desea aproximar: '); 
+function lagrange1()
 
-    x = zeros(1, n+1);
-    y = zeros(1, n+1);
+n = input('Ingrese grado del polinomio de lagrange: ');
 
-    for i = 1:n+1
-        x(i) = input(sprintf('A%d = Ingrese el X: ', i));  
-        y(i) = input(sprintf('A%d = Ingrese el Y: ', i)); 
-    end
+x = []; 
+y = []; 
 
-    yr = 0; 
+for i = 1 : n +1
+    x(i) = input(sprintf('A%d = Ingrese el X: ', i));  
+    y(i) = input(sprintf('A%d = Ingese el Y: ',i)); 
+end
 
-    for i = 1:n+1
-        L = 1;
-        for j = 1:n+1
-            if j ~= i
-                L = L * (xr - x(j)) / (x(i) - x(j));
+band = 1;
+
+while band == 1 
+    xr =input('Ingrese la abscisa intermedia que desea aproximar: '); 
+    yr = 0;
+
+    for i = 1 : n + 1
+
+    j = 1; 
+    NL = 1; 
+    DL = 1; 
+
+    while j <= n 
+        if  (i ~= n) || (i ~= j) 
+            if i == j 
+                j = j + 1; 
             end
+            NL = NL * (xr - x(j)); 
+            DL = DL * (x(i) - x(j)); 
         end
-        yr = yr + L * y(i);
+        j = j + 1; 
     end
 
-    disp(yr);
+        yr = yr + ((NL/DL)*y(i)); 
+
+    end
+
+    disp(yr); 
+
+     continuar = input('¿Desea continuar? (1 = sí, 0 = no): ');
+    
+    if continuar == 0
+        band = 0; 
+    end
+end
+
+
 end
